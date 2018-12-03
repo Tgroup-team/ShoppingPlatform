4dc944675d4436ba4cd2f76b5c06e945860cf95e
liyongchao <1214744492@qq.com>
2018-12-03 13:00:19 +0800

    忽略

--- src/web/dao/util/DbConnection.java
+++ src/web/dao/util/DbConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DbConnection {
     static String url = "jdbc:sqlserver://localhost:1433;DatabaseName=newlifeDB";
-    static String username = "sa";
-    static String password = "root";
+    static String username = "root";
+    static String password = "888999";
     static String driverClassName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     
     public static Connection getConnection() {
