c8afa8a0de9fb897d2c23cb119dc1f4f4169ab5a
admin <admin@192.168.1.37>
2018-12-05 19:33:31 +0800

    个人中心

---
+++ src/web/servlet/user/BrowseServlet.java
@@ -0,0 +1,51 @@
+package web.servlet.user;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import web.dao.ProductDao;
+import web.entity.Product;
+
+/**
+ * Servlet implementation class BrowseServlet
+ */
+@WebServlet("/BrowseServlet")
+public class BrowseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BrowseServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ProductDao productdao=new ProductDao();
+		List<Product> products=productdao.selectProduct();
+		for (Product product : products) {
+			System.out.println(product.toString());
+		}
+		request.setAttribute("products", products);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
